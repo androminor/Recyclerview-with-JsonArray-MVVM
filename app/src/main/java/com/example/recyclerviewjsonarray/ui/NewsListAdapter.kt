6abd56c5c21f8c0357fb2679831a8f95887a7fde
Varun <varun@IM-111618.local>
2021-11-25 13:03:55 +0530

    picture loading stuff

--- app/src/main/java/com/example/recyclerviewjsonarray/ui/NewsListAdapter.kt
+++ app/src/main/java/com/example/recyclerviewjsonarray/ui/NewsListAdapter.kt
@@ -1,41 +1,55 @@
 package com.example.recyclerviewjsonarray.ui
 
 import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.recyclerviewjsonarray.R
-import com.example.recyclerviewjsonarray.model.RecylerData
-import com.squareup.picasso.Picasso
+import com.example.recyclerviewjsonarray.model.RecyclerData
 
-class NewsListAdapter: RecyclerView.Adapter<NewsListAdapter.CustomViewHolder>() {
 
-    var items = ArrayList<RecylerData>()
+class NewsListAdapter(context: Context?) : RecyclerView.Adapter<NewsListAdapter.CustomViewHolder>() {
+
+    private val mContext = context
+    private val TAG = "MyActivity"
+    var items = ArrayList<RecyclerData>()
 
     @SuppressLint("NotifyDataSetChanged")
-    fun setLatestData(items:ArrayList<RecylerData>) {
+    fun setLatestData(items:ArrayList<RecyclerData>) {
         this.items = items
         notifyDataSetChanged()
     }
 
     class CustomViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        private val title:TextView= itemView.findViewById(R.id.title)
-        private val desc:TextView= itemView.findViewById(R.id.description)
-        private val imageBox: ImageView = itemView.findViewById(R.id.imageref)
+        val title:TextView= itemView.findViewById(R.id.title)
+        val desc:TextView= itemView.findViewById(R.id.description)
+        val imageBox: ImageView = itemView.findViewById(R.id.imageref)
 
-        fun bind(data:RecylerData) {
+        fun bind(data: RecyclerData) {
             title.text = data.title
             desc.text = data.description
 
             val url = data.imageHref
 
-            Picasso.get().load(url).into(imageBox)
+                Log.e(TAG,"url : $url")
+         //        Picasso.get().load(url).into(imageBox)
+
+            if(url !=null) {
+                Glide.with(itemView.getContext())
+                    .load(url)
+                    .into(imageBox)
+            }
 
 
-        }
+            };
+
 
     }
 
@@ -49,9 +63,11 @@ class NewsListAdapter: RecyclerView.Adapter<NewsListAdapter.CustomViewHolder>() 
 
     override fun onBindViewHolder(holder:CustomViewHolder, position: Int) {
         holder.bind(items.get(position))
+
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
+
 }
--- app/src/main/java/com/example/recyclerviewjsonarray/ui/NewsListFragment.kt
+++ app/src/main/java/com/example/recyclerviewjsonarray/ui/NewsListFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewjsonarray.R
@@ -30,10 +29,8 @@ class NewsListFragment : Fragment() {
     private fun initViewModel(view: View?) {
     val recyclerView =view?.findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView?.layoutManager = LinearLayoutManager(activity)
-        val decorationItem = DividerItemDecoration(activity,DividerItemDecoration.HORIZONTAL)
-        recyclerView?.addItemDecoration(decorationItem)
 
-        newsAdapter = NewsListAdapter()
+        newsAdapter = NewsListAdapter(context)
         recyclerView?.adapter = newsAdapter
     }
 
